5e29b1b0b90d17d1a3f7882d45a19289b73fb6c8
Bigtoxa766 <bigtoxa766@gmail.com>
2023-11-22 09:21:48 +0200

    main-logic-final

---
+++ src/PixabayAPI.js
@@ -0,0 +1,30 @@
+import Notiflix from 'notiflix';
+
+export class PixabayAPI {
+  API_KEY = '34923818-41fb85465916de5dce0352c63';
+  BASE_URL = 'https://pixabay.com/api/';
+  
+  query = null;
+  page = 1;
+  perPage = 40;
+
+  async fetchPhoto() {
+
+    const params = new URLSearchParams({
+      key: this.API_KEY,
+      q: this.query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: this.page,
+      per_page: this.perPage
+    })
+    try {
+      const resp = await axios(`${this.BASE_URL}?${params}`)
+      const dataPic = await resp.data;
+      return dataPic
+    } catch (err) {
+      Notiflix.Notify.failure(err)
+    }
+}
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,89 @@
-console.log('hello')
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { PixabayAPI } from "./PixabayAPI";
+
+const formEl = document.querySelector('.search-form');
+const galleryEl = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+loadMoreBtn.hidden = true;
+
+const pixabayAPI = new PixabayAPI();
+
+formEl.addEventListener('submit', onSearch);
+loadMoreBtn.addEventListener('click', onClick)
+
+function onSearch(e) {
+  e.preventDefault();
+
+  const searchQuery = e.target.elements.searchQuery.value;
+  pixabayAPI.query = searchQuery;
+
+  pixabayAPI.fetchPhoto().then(data => {
+
+    if (data.hits.length === 0) {
+      Notiflix.Notify.warning("Sorry, there are no images matching your search query. Please try again.")
+    }
+
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`)
+    
+    formEl.reset();
+    galleryEl.innerHTML = '';
+    renderCard(data.hits);
+    loadMoreBtn.hidden = false;
+
+    if (data.hits.length === 0 || data.hits.length < pixabayAPI.perPage) {
+        loadMoreBtn.hidden = true;
+        return Notiflix.Notify.info(`We're sorry, but you've reached the end of search results.`);
+    }
+   
+  })
+};
+
+function renderCard(arr) {
+ return arr.map(({ webformatURL, largeImageURL, tags, likes,
+  views, comments, downloads}) => {
+    const markup = `
+<div class="photo-card">
+  <div class="photo-wrapper"><img src="${webformatURL}" alt="${tags}" loading="lazy" width="350px"/></div>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      ${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+      ${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+      ${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+      ${downloads}
+    </p>
+  </div>
+</div>`;
+
+    galleryEl.insertAdjacentHTML('beforeend', markup)
+  }).join()
+};
+
+function onClick() {
+  
+  pixabayAPI.page += 1;
+  pixabayAPI.fetchPhoto()
+    .then(data => {
+      renderCard(data.hits)
+
+      if (data.hits.length === 0 || data.hits.length < pixabayAPI.perPage) {
+        loadMoreBtn.hidden = true;
+        return Notiflix.Notify.info(`We're sorry, but you've reached the end of search results.`);
+    }
+    })
+};
+
+
+
